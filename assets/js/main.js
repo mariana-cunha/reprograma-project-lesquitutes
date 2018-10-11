f705cd025a56a3cc833e520abd30860bcc1c27aa
mariana <mari.marques26@gmail.com>
2018-10-11 02:44:31 -0300

    update final changes from may

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,82 @@
-function onClickCategory (productCategory) {
-  if ( productCategory === 'cakes') {
-    window.location.href = 'cakes.html';
-  }
-  if ( productCategory === 'desserts') {
-    window.location.href = 'desserts.html';
-  }
+function onClickCategory(productCategory) {
+    if (productCategory === 'cakes') {
+        window.location.href = 'cakes.html';
+    }
+    if (productCategory === 'desserts') {
+        window.location.href = 'desserts.html';
+    }
 }
 
-function onClickProductType (productCategory, productType) {
-  console.log(productType, productCategory);
+function onClickProductType(productCategory, productType) {
+    console.log(productType, productCategory);
 
-  if (productCategory === 'desserts-array') {
-    constructorHTML(desserts, productType);
-  }
+    if (productCategory === 'desserts-array') {
+        constructorHTML(desserts, productType);
+    }
 
-  if (productCategory === 'cakes-array') {
-    constructorHTML(cakes, productType);
-  }
+    if (productCategory === 'cakes-array') {
+        constructorHTML(cakes, productType);
+    }
 }
 
-function constructorHTML (categories, productType) {
-  if (divCardsProducts.hasChildNodes()) {
-    while (divCardsProducts.firstChild) {
-      divCardsProducts.removeChild(divCardsProducts.firstChild);
-    }
-  }
-
-  let i = 0;
-  categories.filter(element => element.type === productType).forEach(element => {
-    const divCard = document.createElement('div');
-
-    if (element.brief !== '') {
-      i++;
-      divCard.className = `card les-card-double`;
-      divCard.innerHTML = `
-        <img class='card-img-top' src=${element.src}>
-        <div class='card-body'>
-          <h6 class='card-title'>${element.description}</h6>
-          <p>${element.brief}</p>
-          ${element.tags.map(tag => `<p>${tag}</p>`).join('')}
-        </div>
-      `;
-    } else {
-      divCard.className = 'card les-card-single';
-      divCard.innerHTML = `
-        <img class='card-img-top' src=${element.src}>
-        <div class='card-body'>
-          <h6 class='card-title'>${element.description}</h6>
-          ${element.tags.map(tag => `<p>${tag}</p>`).join('')}
-        </div>
-      `;
+function constructorHTML(categories, productType) {
+    if (divCardsProducts.hasChildNodes()) {
+        while (divCardsProducts.firstChild) {
+            divCardsProducts.removeChild(divCardsProducts.firstChild);
+        }
     }
 
-    divCardsProducts.appendChild(divCard);
-  });
+    switch (productType) {
+        case 'cupcake':
+            document.querySelector('.product-title').textContent = 'Cupcakes';
+            break;
+        case'dessert':
+            document.querySelector('.product-title').textContent = 'Docinhos';
+            break;
+        case'souvenir':
+            document.querySelector('.product-title').textContent = 'Lembrancinhas';
+            break;
+        case'decorated':
+            document.querySelector('.product-title').textContent = 'Bolos decorados';
+            break;
+        case'naked-cake':
+            document.querySelector('.product-title').textContent = 'Naked Cake';
+            break;
+        case'american-pasta':
+            document.querySelector('.product-title').textContent = 'Pasta americana';
+            break;
+    }
+
+    categories.filter(element => element.type === productType).forEach(element => {
+        const divCard = document.createElement('div');
+
+        if (element.brief !== '') {
+            divCard.className = 'card les-card-double d-flex align-items-stretchs';
+            divCard.innerHTML = `
+            <img class='img-fluid h-100' src=${element.src}>
+            <div class='card-body position-absolute w-50'>
+                <p class='text-shadow card-text text-light card-headline'>${element.brief}</p>
+                <p class='text-shadow card-text- text-light card-tagline mb-2'>${element.author}</p>
+
+                ${element.tags.map(tag => `<img class='stamp-card' src='${tag}'>`).join('')}
+            </div>
+            <div class='card-text bg-light'>
+                <p class='text-center mt-2 mb-2'>${element.description}</p>
+            </div>
+          `;
+        } else {
+            divCard.className = 'card les-card-single d-flex align-items-stretch';
+            divCard.innerHTML = `
+            <img class='img-fluid h-100' src=${element.src}>
+            <div class='card-body position-absolute w-50'>
+            ${element.tags.map(tag => `<img class='stamp-card' src='${tag}'>`).join('')}
+            </div>
+            <div class='card-text bg-light'>
+              <p class='text-center mt-2 mb-2'>${element.description}</p>
+            </div>
+            `;
+        }
+
+        divCardsProducts.appendChild(divCard);
+    });
 }
